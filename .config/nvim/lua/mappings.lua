565d3eb559fcd9b0331d8f3a007f3ab9d3114868
sunpnwt12 <sun.punnawit@gmail.com>
2022-09-10 16:35:05 +0700

    Updated tmux and nvim key-bind

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -1,33 +1,38 @@
 local map = vim.keymap.set
-local opts = {noremap = true, silent = true}
+local defaults_opts = { noremap = true, silent = true }
 
 -- command
 vim.cmd [[command! Wd write|bdelete]]
 
 -- remap
-map('n', '<leader>o', 'o<Esc>', opts)
-map('n', '<leader>O', 'O<Esc>', opts)
-map('n', 'Y', 'y$', opts)
+--
+-- new line without leaving normal mode
+map('n', '<leader>o', 'o<Esc>', defaults_opts)
+map('n', '<leader>O', 'O<Esc>', defaults_opts)
+
+-- yank the rest of the line
+map('n', 'Y', 'y$', defaults_opts)
 
 --moving lines
-map('n', '<A-j>', ':m .+1<CR>==', opts)
-map('n', '<A-k>', ':m .-2<CR>==', opts)
-map('v', '<A-j>', ':m \'>+1<CR>gv=gv', opts)
-map('v', '<A-k>', ':m \'<-2<CR>gv=gv', opts)
+map('n', '<a-j>', ':m .+1<cr>==', defaults_opts)
+map('v', '<a-j>', ':m \'>+1<cr>gv=gv', defaults_opts)
+map('n', '<A-k>', ':m .-2<CR>==', defaults_opts)
+map('v', '<A-k>', ':m \'<-2<CR>gv=gv', defaults_opts)
+
 -- telescope
-map('n', '<leader>ff', ':Telescope find_files<CR>', opts)
-map('n', '<leader>fg', ':Telescope live_grep<CR>', opts)
+map('n', '<leader>ff', ':Telescope find_files find_command=rg,--ignore,--hidden,--files<CR>', defaults_opts)
+map('n', '<leader>fg', ':Telescope live_grep<CR>', defaults_opts)
 
 -- nvim-tree
-map('n', '<leader>e', ':NvimTreeToggle<CR>', opts)
+map('n', '<leader>e', ':NvimTreeToggle<CR>', defaults_opts)
 
 -- bufferline
-map('n', '<S-l>', ':BufferLineCycleNext<CR>', opts)
-map('n', '<S-h>', ':BufferLineCyclePrev<CR>', opts)
+map('n', '<S-l>', ':BufferLineCycleNext<CR>', defaults_opts)
+map('n', '<S-h>', ':BufferLineCyclePrev<CR>', defaults_opts)
 
 -- trouble
-map("n", "<leader>xx", ":Trouble<CR>", opts)
-map("n", "<leader>xw", ":Trouble workspace_diagnostics<CR>", opts)
-map("n", "<leader>xd", ":Trouble document_diagnostics<CR>", opts)
-map("n", "<leader>xq", ":Trouble quickfix<CR>", opts)
-map("n", "gR", ":Trouble lsp_references<CR>", opts)
+map("n", "<leader>xx", ":Trouble<CR>", defaults_opts)
+map("n", "<leader>xw", ":Trouble workspace_diagnostics<CR>", defaults_opts)
+map("n", "<leader>xd", ":Trouble document_diagnostics<CR>", defaults_opts)
+map("n", "<leader>xq", ":Trouble quickfix<CR>", defaults_opts)
+map("n", "gR", ":Trouble lsp_references<CR>", defaults_opts)
--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -60,7 +60,8 @@ return require('packer').startup(function(use)
 
     use {
         'nvim-telescope/telescope.nvim', tag = '0.1.0',
-        requires = { { 'nvim-lua/plenary.nvim' } }
+        requires = { { 'nvim-lua/plenary.nvim' } },
+        config = get_config('telescope')
     }
 
     use { 'Mofiqul/dracula.nvim',
