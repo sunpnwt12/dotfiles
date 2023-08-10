9592bee942c0d194f587f3f1e5c5ee3580d1767a
Punnawit Metheenopanant <sun.punnawit@gmail.com>
2023-08-11 01:38:27 +0700

    Added new way to config using dotter & Added full fish config path

--- fish/config.fish
+++ fish/config.fish
@@ -15,11 +15,16 @@ function cs
     curl cheat.sh/$argv[1]/$argv[2]
 end
 
+function mb
+    micromamba $argv
+end
+
 # alias
 alias ls="exa -la --icons"
 alias lg="lazygit"
 alias nv="nvim"
 alias ipy="ipython3" 
+# alias mb='mircomamba'
 alias tmux="tmux -u"
 
 set -gx BAT_THEME "Catppuccin-mocha"
@@ -28,3 +33,10 @@ set -gx EDITOR "nvim"
 starship init fish | source
 zoxide init fish | source
 
+
+# >>> mamba initialize >>>
+# !! Contents within this block are managed by 'mamba init' !!
+set -gx MAMBA_EXE "/home/sunpnwt/bin/micromamba"
+set -gx MAMBA_ROOT_PREFIX "/home/sunpnwt/micromamba"
+$MAMBA_EXE shell hook --shell fish --root-prefix $MAMBA_ROOT_PREFIX | source
+# <<< mamba initialize <<<
