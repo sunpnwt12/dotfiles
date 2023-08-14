64cf820cd1a30975ba87aa9c2b05945902148cfe
Punnawit Metheenopanant <sun.punnawit@gmail.com>
2023-08-14 14:21:44 +0700

    Removed telescope-undo as it did not work

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -6,26 +6,6 @@ local config = function()
 			zoxide = {
 				prompt_title = "Zoxide",
 			},
-			undo = {
-				use_delta = true,
-				use_custom_command = nil, -- setting this implies `use_delta = false`. Accepted format is: { "bash", "-c", "echo '$DIFF' | delta" }
-				side_by_side = false,
-				diff_context_lines = vim.o.scrolloff,
-				entry_format = "state #$ID, $STAT, $TIME",
-				time_format = "",
-				mappings = {
-					i = {
-						-- IMPORTANT: Note that telescope-undo must be available when telescope is configured if
-						-- you want to replicate these defaults and use the following actions. This means
-						-- installing as a dependency of telescope in it's `requirements` and loading this
-						-- extension from there instead of having the separate plugin definition as outlined
-						-- above.
-						["<cr>"] = require("telescope-undo.actions").yank_additions,
-						["<S-cr>"] = require("telescope-undo.actions").yank_deletions,
-						["<C-cr>"] = require("telescope-undo.actions").restore,
-					},
-				},
-			},
 		},
 
 		pickers = {
@@ -42,7 +22,6 @@ local config = function()
 		},
 	})
 	telescope.load_extension("zoxide")
-	telescope.load_extension("undo")
 end
 
 return {
